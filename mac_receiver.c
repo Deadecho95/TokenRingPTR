61dabc7b87a488dc54785f161e1148ab35f72058
Florence Villoz <florence.villoz@students.hevs.ch>
2018-12-03 16:18:11 +0100

    begin mac receiver

--- mac_receiver.c
+++ mac_receiver.c
@@ -20,12 +20,15 @@ void MacReceiver(void *argument)
 	struct queueMsg_t queueMsg;		// queue message
 	uint8_t * msg;
 	uint8_t * qPtr;
+	uint8_t crc = 0;
 	size_t	size;
 	osStatus_t retCode;
 	
+	
+	
 	//------------------------------------------------------------------------------
 	for (;;)						// loop until doomsday
-	{
+	{	
 		//----------------------------------------------------------------------------
 		// QUEUE READ										
 		//----------------------------------------------------------------------------
@@ -36,20 +39,51 @@ void MacReceiver(void *argument)
 			osWaitForever); 	
     CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);				
 	//----------------------------------------------------------------------------
-	// MEMORY ALLOCATION				
+	// Test Frame				
 	//----------------------------------------------------------------------------
-	msg = osMemoryPoolAlloc(memPool,osWaitForever);
-	if(queueMsg.type == NEW_TOKEN){
-		qPtr = (uint8_t*)&queueMsg + 8;
-	}
-	
-	memcpy(msg,&qPtr[1],size-2);
-
-									
-	
-	queueMsg.anyPtr = msg;
-	queueMsg.type = FROM_PHY;
+		qPtr = queueMsg.anyPtr;
+		if(qPtr[0] == TOKEN_TAG){							// token frame 
+			retCode = osMessageQueuePut(
+				queue_macS_id,
+				&queueMsg,
+				NULL,
+				osWaitForever);
+		}
+		else{ 			// data frame
+			//crc//
+			for(int i = 0; i < qPtr[2]; i++){
+				crc = crc + *qPtr+2+i;
+			}
+			crc = crc%6;
+			//crcr not ok
+			if(crc != ((qPtr[qPtr[2]+3])&0b11111100)>>2){
+				qPtr[qPtr[2]+3] = qPtr[qPtr[2]+3] | 0b00000010;	//READ = 1
+				retCode = osMessageQueuePut(
+					queue_phyS_id,
+					&queueMsg,
+					NULL,
+					osWaitForever);
+			}
+			//crc ok
+			else{		//data frame ok
+				qPtr[qPtr[2]+3] = qPtr[qPtr[2]+3] | 0b00000011;	//READ = 1 ack = 1
+				switch(){
+					case :
+						break;
+					
+					default:
+						break;
+				}
+			}
+			
+				
+		//----------------------------------------------------------------------------
+		// Mem alloc				
+		//----------------------------------------------------------------------------
+			msg = osMemoryPoolAlloc(memPool,osWaitForever);											
+			memcpy(msg,&qPtr[1],size-2);
+			queueMsg.anyPtr = msg;
+		}
 
 	}
 }
-
