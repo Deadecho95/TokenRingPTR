fe6180eefe96cef38c5b2ebda027b68d7d30a3be
Florence Villoz <florence.villoz@students.hevs.ch>
2018-12-05 14:25:40 +0100

    macr

--- mac_receiver.c
+++ mac_receiver.c
@@ -18,6 +18,7 @@
 void MacReceiver(void *argument)
 {
 	struct queueMsg_t queueMsg;		// queue message
+	osMessageQueueId_t queue;
 	uint8_t * msg;
 	uint8_t * qPtr;
 	uint8_t crc = 0;
@@ -42,7 +43,8 @@ void MacReceiver(void *argument)
 	// Test Frame				
 	//----------------------------------------------------------------------------
 		qPtr = queueMsg.anyPtr;
-		if(qPtr[0] == TOKEN_TAG){							// token frame 
+		if(qPtr[0] == TOKEN_TAG){						// token frame
+			queueMsg.type = TOKEN;
 			retCode = osMessageQueuePut(
 				queue_macS_id,
 				&queueMsg,
@@ -54,10 +56,10 @@ void MacReceiver(void *argument)
 			for(int i = 0; i < qPtr[2]; i++){
 				crc = crc + *qPtr+2+i;
 			}
-			crc = crc%6;
+			crc = crc&0x3f;
 			//crcr not ok
-			if(crc != ((qPtr[qPtr[2]+3])&0b11111100)>>2){
-				qPtr[qPtr[2]+3] = qPtr[qPtr[2]+3] | 0b00000010;	//READ = 1
+			if(crc != ((qPtr[qPtr[2]+3])&0xfc)>>2){
+				qPtr[qPtr[2]+3] = qPtr[qPtr[2]+3] | 0x02;	//READ = 1
 				retCode = osMessageQueuePut(
 					queue_phyS_id,
 					&queueMsg,
@@ -66,13 +68,55 @@ void MacReceiver(void *argument)
 			}
 			//crc ok
 			else{		//data frame ok
-				qPtr[qPtr[2]+3] = qPtr[qPtr[2]+3] | 0b00000011;	//READ = 1 ack = 1
-				switch(){
-					case :
-						break;
-					
-					default:
-						break;
+				qPtr[qPtr[2]+3] = qPtr[qPtr[2]+3] | 0x03;	//READ = 1 ack = 1
+				//source of not
+				if((qPtr[0]&0xf8>>3) == gTokenInterface.myAddress){//if source
+					//if dest or not
+					if((qPtr[1]&0xf8>>3) == gTokenInterface.myAddress || (qPtr[1]&0xf8>>3) == BROADCAST_ADDRESS ){//if dest
+						//sapi chat
+						if((qPtr[1]&0x07) == CHAT_SAPI && gTokenInterface.connected == 1 ){//chat
+							queue = queue_chatR_id;
+						}
+						else{
+							queue = queue_timeR_id;
+						}//time
+					  //--------------
+						//databack
+						//--------------
+						queueMsg.type = DATABACK;
+						queueMsg.addr = (qPtr[0]&0x78)>>3;
+						queueMsg.sapi = (qPtr[0]&0x07);
+						retCode = osMessageQueuePut(
+							queue_macS_id,
+							&queueMsg,
+							NULL,
+							osWaitForever);
+						//----------------------------------------------------------------------------
+						// Mem alloc				
+						//----------------------------------------------------------------------------
+							msg = osMemoryPoolAlloc(memPool,osWaitForever);											
+							memcpy(msg,&qPtr[3],size-1);
+							queueMsg.anyPtr = msg;
+							queueMsg.type = DATA_IND;
+							queueMsg.addr = (qPtr[0]&0x78)>>3;
+							queueMsg.sapi = (qPtr[0]&0x07);
+						//--------------------------------------------------------------------------
+						// QUEUE SEND	(send received frame to chat reciver)
+						//--------------------------------------------------------------------------
+						retCode = osMessageQueuePut(
+							queue,
+							&queueMsg,
+							osPriorityNormal,
+							osWaitForever);
+						CheckRetCode(retCode,__LINE__,__FILE__,CONTINUE);		
+						
+					}
+					else{//not dest
+						//databack
+					}
+				}
+				else{//not source
+					//todo
 				}
 			}
 			
